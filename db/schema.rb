fa41cb9f8b8435dd4a95f12dbd867a2091851fe7
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-10-26 22:02:29 +0000

    [Add]model CartItem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_215707) do
+ActiveRecord::Schema.define(version: 2022_10_26_220026) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2022_10_26_215707) do
   create_table "admins", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
+    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
