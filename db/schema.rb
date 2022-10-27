1ffa172b3dd30a2749436fabd0801f931d1a1564
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-10-27 11:52:56 +0000

    [Add]model Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_220026) do
+ActiveRecord::Schema.define(version: 2022_10_27_114050) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -68,6 +68,19 @@ ActiveRecord::Schema.define(version: 2022_10_26_220026) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
