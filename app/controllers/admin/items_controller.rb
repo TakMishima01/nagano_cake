530fd50cf9377fa17191276f5aab5b6cc0c75756
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-11-04 22:03:19 +0000

    [Add]Item showページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,15 +9,26 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item)
   end
 
   def show
+    @item = Item.find(params[:id])
+    @add_tax = @item.price * 1.1.ceil
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :genre_id, :name, :introduction, :price, :is_active)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :genre
 end
+  def add_tax_price
+    (@item.price * 1.10).round
+  end
+
+  def show_status
+    if (@item.is_active) == "ture"
+      "販売中"
+    else
+      "販売停止中"
+    end
+  end
