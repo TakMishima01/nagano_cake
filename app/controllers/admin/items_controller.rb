9e1220726d7a5d3c08290a5b0a59a44492d0202e
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-11-04 22:24:14 +0000

    [Add]Item editページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,11 +20,15 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id]) 
+    @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
   end
-  
+
   private
 
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,3 @@ end
   def add_tax_price
     (@item.price * 1.10).round
   end
-
-  def show_status
-    if (@item.is_active) == "ture"
-      "販売中"
-    else
-      "販売停止中"
-    end
-  end
