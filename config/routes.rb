c0fd264a1514f203cd314a997a0f547a47df5057
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-11-10 21:32:19 +0000

    [test]

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
 
 root to: "public/homes#top"
 get '/about' => "public/homes#about", as: 'about'
-
- devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
 
 scope module: :public do
     resources :items, only: [:index, :show]
@@ -21,10 +24,11 @@ namespace :admin do
   root 'homes#top'
   resources :genres, only: [:index, :create, :edit, :update]
   resources :items, except: [:destroy]
+  resources :customers, only: [:index, :show, :edit, :update]
 end
 
 
 
- 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
