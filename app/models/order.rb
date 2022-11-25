43cf13e08fa1ac46eaaf0993652abc3491d76467
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-11-25 21:51:08 +0000

    [end]order_complete,confirm,show,indexの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_payment: 0, payment_confirmed: 1, making: 2, shipping_preparation: 3, sent: 4 }
+
+  def subtotal
+    item.add_tax_price*amount
+  end
 end
