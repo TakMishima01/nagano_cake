d17489d1b41c06a16c47f4cf7edf017016f66ad8
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2022-11-29 13:08:48 +0000

    [Add]キーワード検索の追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,23 +1,38 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @genres = Genre.all
-    @title = "商品"
-    @items_count = Item.count
-    @items = Item.page(params[:page]).per(8)
+    # @genres = Genre.all
+    # @title = "商品"
+    # @items_count = Item.count
+    # @items = Item.page(params[:page]).per(8)
 
-    if params[:genre_id].present?
+    # if params[:genre_id].present?
+    #   @genre = Genre.find(params[:genre_id])
+    #   @title = @genre.name
+    #   @items_count = @genre.items.count
+    #   @items = @genre.items.page(params[:page]).per(8)
+    # end
+  @genres = Genre.all
+    if params[:q].blank? && params[:genre_id].blank?
+      @title = "商品"
+      @items_count = @search_items.count
+      @items = @search_view
+    elsif params[:q].present? && params[:genre_id].blank?
+      @title = "検索結果"
+      @items_count = @search_items.count
+      @items = @search_view
+    else params[:q].blank? && params[:genre_id].present?
       @genre = Genre.find(params[:genre_id])
       @title = @genre.name
       @items_count = @genre.items.count
       @items = @genre.items.page(params[:page]).per(8)
     end
+
+
+
+
+
   end
-
-  def search
-  end
-
-
 
   def show
     @genres = Genre.all
